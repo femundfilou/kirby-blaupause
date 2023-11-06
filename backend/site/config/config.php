211e6557afae5d70fd2fc0df68fa77426238cf5d
Justus Kraft <hallo@justuskraft.de>
2023-11-06 23:33:03 +0100

    feat: update config, add optional translations route

--- backend/site/config/config.php
+++ backend/site/config/config.php
@@ -1,5 +1,32 @@
 <?php
 return [
+	"date"  => [
+		"handler" => "intl"
+	],
+	"debug" => false,
 	"languages" => true,
-	"debug" => true
+	"routes" => [
+		/*
+		* Route for translations
+		* Intended to be used with svelte-i18n
+		[
+			'pattern' => '/v1/translations/(:alpha)',
+			'method' => 'GET',
+			'action' => function ($lang) {
+				try {
+					$translations = kirby()->language($lang)?->translations();
+					if (!$translations) throw new Exception("No translations found for '$lang'.", 404);
+					return Response::json(
+						$translations,
+						200
+					);
+				} catch (Exception $e) {
+					return Response::json(
+						$e->getMessage(),
+						$e->getCode()
+					);
+				}
+			},
+		],
+		*/]
 ];
