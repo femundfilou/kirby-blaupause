bce52e1aeecb7ef6effc9f605c245294f09fc258
Justus Kraft <hallo@justuskraft.de>
2024-11-29 09:31:31 +0100

    feat: add icon shorthand

--- backend/site/plugins/blaupause-helper/index.php
+++ backend/site/plugins/blaupause-helper/index.php
@@ -1,6 +1,20 @@
 <?php
+
+use Kirby\Template\Snippet;
+
 Kirby\Filesystem\F::loadClasses([
 	'femundfilou\\menu\\menu' => 'lib/Menu.php'
 ], __DIR__);
+
+/**
+ * Returns the icon snippet
+ * @param string $name Name of the icon
+ * @param string $size Size of the icon
+ * @return Snippet|string|null
+ */
+function icon(string $name, string $size = '1em')
+{
+	return snippet('icon', ['name' => $name, 'size' => $size], true);
+}
 
 Kirby\Cms\App::plugin('femundfilou/blaupause-helper', []);
